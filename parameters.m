e242b22ee46ad38136f71c345c64b38578392178
MatGrav <matteo.gravagnone@gmail.com>
2024-05-11 18:51:06 +0200

    First approach: parameters, initial model and script

---
+++ parameters.m
@@ -0,0 +1,42 @@
+clear all; clc; clear;
+
+%% Parameters
+e_machine_peak_power = 150*10^3; % Watt
+e_machine_max_torque = 310; % Nm
+e_machine_max_speed = 16000; % rpm
+gear_ratio = 10.5;
+e_powertrain_pure_time_delay = 20; %ms
+e_machine_torque_time_constant = 50; %ms
+
+e_motor_inverter_efficiency = 0.90;
+transmission_efficiency = 0.95;
+
+torsional_stiffness = 9000; % Nm/rad
+battery_capacity = 58; % kWh
+battery_voltage = 800; % V
+
+% Tyre rolling resistance coefficients
+f0 = 0.009;
+% f1 = 0
+f2 = 6.5 * 10^-6; % s^2 / m^2
+
+L_rel = 0.285; % Tyre relax. length (range between 0.12m and 0.45)
+
+brakes_generation_deadtime = 20; %ms
+brakes_friction_rise_time = 25; %ms
+brakes_torque_distribution = 3.0; % 75:25
+
+kerb_weight = 1812; % kg
+wheelbase = 2.77; % m
+CoG_height = 0.55; % m
+
+FtoR_mass_distribution = 1; % 50:50
+S = 2.36; % m^2 % cross section / frontal area
+Cx = 0.27; % aerodinamic drag coefficient
+
+% Wheel mass moment of inertia [kg m^2]
+Jns = 0.8;
+
+
+
+save vehicle_parameters.mat
---
+++ script.m
@@ -0,0 +1,16 @@
+clear all; close all; clc
+
+%% Parameters initialization
+run('Pacejka for Homework\Load_Tyre_Data.m')
+load('vehicle_parameters.mat')
+
+velstart = 0;
+wheel_radius = 359; % mm
+
+rho = 1.204; % air density [kg/m3] at 20°C
+%%
+
+inclination = 0;
+
+Tsim = 100;
+open("model.slx");
